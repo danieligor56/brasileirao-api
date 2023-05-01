c748a717ca111db9a948b19ece234ab24ca7ec00
Daniel Igor <danieligorxd@gmail.com>
2023-05-01 19:54:11 -0300

    Criação do DTO com dados da partida,Criação de Enums de Status da partida e alguns ajustes.

--- src/main/java/brasileiraoapi/dto/PartidaGoogleDto.java
+++ src/main/java/brasileiraoapi/dto/PartidaGoogleDto.java
@@ -2,10 +2,14 @@ package brasileiraoapi.dto;
 
 import java.io.Serializable;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class PartidaGoogleDto implements Serializable {
 
 	private static final long serialVersionUID = 1L;
